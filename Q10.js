072a3c42f89ff3a304d23b14e49bbf0d1f7bca75
farooqrimsha123 <159934322+farooqrimsha123@users.noreply.github.com>
2024-07-08 23:18:25 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,6 @@
+// Adding Comments:Choose two of the programs you've written,and add at least one comment to each. If you don't have anything specific to write because your programs are too simple at this point,just add your name and the current date at the top of each program File.Thn write one sentence describing what the program does.
+//exercise 10
+// my name is Rimsha
+// Dated:04/03/2024
+// this program will run simple code just like hello friends
+console.log('hello friends simple program');
